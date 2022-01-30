962d1bef5a28a4c666fa3e8147cff484b005d1fa
Junya Qiao <62684140+JunyaQ@users.noreply.github.com>
2022-01-29 22:01:56 -0500

    create seed

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -24,29 +24,37 @@ db.once('open', async () => {
   password:"123321"
  }])
 
+ 
  const categories = await Category.insertMany([{
-   catname:"Hot Coffee",
+   catname:"Hot Coffee",//0
    //subcatname:"first-subcat",
  },
  {
-  catname:"Cold Coffee",
+  catname:"Cold Coffee",//1
   //subcatname:"first-second-subcat",
 },
-
 {
-  catname:"Hot tea",
+  catname:"Hot tea",//2
   //subcatname:"second-subcat",
-
 },
 {
-  catname:'cold tea',
+  catname:'cold tea',//3
 }])
 //console.log(categories);
+
+
 await Drink.insertMany([
   {
-    drinkname:"cold brew",
+    drinkname:"Cafe Misto",
     size:"grande",
     price:1.23,
+    category:[categories[0]._id],
+    //addin:''
+  },
+  {
+    drinkname:'Pike Place Roast',
+    size:'grande',
+    price:2.23,
     category:[categories[0]._id],
     //addin:''
   },
@@ -54,16 +62,195 @@ await Drink.insertMany([
     drinkname:'Americano',
     size:'grande',
     price:2.23,
-    category:[categories[1]._id],
+    category:[categories[0]._id],
     //addin:''
   },
   {
-    drinkname:'test third drink',
+    drinkname:'Brow Sugar Oat Americano',
+    size:'grande',
+    price:2.23,
+    category:[categories[0]._id],
+    //addin:''
+  },
+  {
+    drinkname:'Iris Cream Americano',
+    size:'grande',
+    price:2.23,
+    category:[categories[0]._id],
+    //addin:''
+    
+  },
+  {
+    drinkname:'Cappuccino',
+    size:'grande',
+    price:2.23,
+    category:[categories[0]._id],
+    //addin:''
+  },
+  {
+    drinkname:'Esoresso',
+    size:'grande',
+    price:2.23,
+    category:[categories[0]._id],
+    //addin:''
+  },
+  {
+    drinkname:'Flat White',
+    size:'grande',
+    price:2.23,
+    category:[categories[0]._id],
+    //addin:''
+  },
+  {
+    drinkname:'Irish Cream Cold Brew',
     size:'tall',
     price:1.11,
     category:[categories[1]._id],
-  }
-  
+  },
+  {
+    drinkname:'Salted Caramel Cold Brew',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Vanilla Sweet cream Cold Brew',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Cold Brew',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Iced Americano',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Nitro Cold Brew',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Vanilla Sweet Cream Nitro Cold Crew',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Iced Coffee',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Iced Shaken Espresso',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Iced Honey Flat White',
+    size:'tall',
+    price:1.11,
+    category:[categories[1]._id],
+  },
+  {
+    drinkname:'Chai Tea',
+    size:'grande',
+    price:1.50,
+    category:[categories[2]._id],
+  },
+  {
+    drinkname:'Chai Tea Latte',
+    size:'grande',
+    price:1.50,
+    category:[categories[2]._id],
+  },
+  {
+    drinkname:'London Fog Tea Latte',
+    size:'grande',
+    price:1.50,
+    category:[categories[2]._id],
+  },
+  {
+    drinkname:'Earl Grey Tea',
+    size:'grande',
+    price:1.50,
+    category:[categories[2]._id],
+  },
+  {
+    drinkname:'Royal English Breakfast Tea',
+    size:'grande',
+    price:1.50,
+    category:[categories[2]._id],
+  },
+  {
+    drinkname:'Matcha Tea Latte',
+    size:'grande',
+    price:1.50,
+    category:[categories[2]._id],
+  },
+  {
+    drinkname:'Honey Citruc Mint Tea',
+    size:'grande',
+    price:1.50,
+    category:[categories[2]._id],
+  },
+  {
+    drinkname:'Iced Chai Tea Latte',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
+  {
+    drinkname:'Iced Chai Tea Latte',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
+  {
+    drinkname:'Iced Black Tea',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
+  {
+    drinkname:'Iced Black Tea Lemonade',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
+  {
+    drinkname:'Iced Matcha Tea Latte',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
+  {
+    drinkname:'Iced Peach Green Tea',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
+  {
+    drinkname:'Iced Green Tea',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
+  {
+    drinkname:'Iced Peach Green Tea Lemonade',
+    size:'grande',
+    price:1.50,
+    category:[categories[3]._id],
+  },
 ])
 
 //cart data (before mutation)
