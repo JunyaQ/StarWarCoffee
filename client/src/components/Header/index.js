0251636491bd99f6926b32f320a50fdbf63fd9dd
Junya Qiao <62684140+JunyaQ@users.noreply.github.com>
2022-01-29 23:38:26 -0500

    add pic for header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../../img/logo.jpg'
 
 const Header = () => {
   return (
     <section>
     <header className="bg-success mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
+      <img src={logo} alt="Logo" className='logo'/>
         <h1>Star Wars Coffee</h1>
       </div>
     </header>
